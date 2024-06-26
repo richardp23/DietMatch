4dca22c7655b1e57f09e10282b92cbbd9d8dc2a4
Richard Perez Jr <richard.p23@icloud.com>
2024-06-26 19:26:47 +0000

    Attempted PEP8 compliance 3

--- openai_integration.py
+++ openai_integration.py
@@ -11,13 +11,12 @@ def alt_recipe_query(key, diet, recipe):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": 
+            {"role": "system", "content":
                 "You are a skilled dietian who can provide any type of"
                 " cooking recipes and alternatives for different diets. "
                 "I am looking to make the following recipe, with alternative"
                 f" ingredients that fit into a {diet} diet."},
             {"role": "user", "content": recipe}
-            # {"role": "user", "content": "What can I substitute beef in a Big Mac with as a vegan?"}
         ]
     )
     return completion.choices[0].message.content
