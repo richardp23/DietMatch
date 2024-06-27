c2f7a32c8dfcaf88ab67e4de83c8371b43ec160b
Richard Perez Jr <richard.p23@icloud.com>
2024-06-27 17:39:55 +0000

    Added user menu

--- main.py
+++ main.py
@@ -15,14 +15,37 @@ OPENAI_KEY = os.getenv('OPENAI_KEY')
 def main():
     print("Welcome to DietMatch!")
 
+    while True:
+        print("What would you like to do?")
+
+        choice = input(
+            "Type \"make\" to make a recipe,"
+            " \"lookup\" to find a previously made recipe,"
+            " or \"exit\" to quit the program! "
+        )
+
+        if choice.lower() == "make":
+            make_recipe()
+        elif choice.lower() == "lookup":
+            lookup_prev_recipe()
+        elif choice.lower() == "exit":
+            exit
+        else:
+            print("Choice not valid, please try again!\n")
+
+
+def make_recipe():
     diet = input("\nPlease tell me what kind of diet you follow: ")
     requested_recipe = input(
         "Please tell me what kind of"
         " dish you would like to make: ")
 
     recipe_query = get_recipe(EDAMAM_APP_ID, EDAMAM_APP_KEY, requested_recipe)
-    print(f"\n{alt_recipe_query(OPENAI_KEY, diet, recipe_query)}")
+    print(f"\n{alt_recipe_query(OPENAI_KEY, diet, recipe_query)}\n") 
 
+
+def lookup_prev_recipe():
+    return True
 
 if __name__ == "__main__":
     main()
