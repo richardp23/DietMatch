2f03bda644a0cf53b1401b001c3868044d9eca67
Sebastian Guevara <sg988170@gmail.com>
2024-07-11 12:31:46 -0700

    Sebastian front end commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/create-recipe")
+def create_recipe():
+    return render_template('create_recipe.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=3000)
