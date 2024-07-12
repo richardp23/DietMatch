1686ce38ff9bfafab446e783cec698a4af062966
Sebastian Guevara <sg988170@gmail.com>
2024-07-11 22:18:56 -0700

    Added previous recipes

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def create_recipe():
     return render_template('create_recipe.html')
 
+@app.route("/previous-recipes")
+def previous_recipes():
+    return render_template('previous_recipes.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=3000)
